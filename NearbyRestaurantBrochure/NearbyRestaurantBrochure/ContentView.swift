c2c8cd128cbb3caf165f1cef0538ceced57642a4
Githubのユーザー名 <syoudai06180502@gmail.com>
2026-02-17 16:21:30 +0900

    Fetch nearby shops using current location

--- NearbyRestaurantBrochure/NearbyRestaurantBrochure/ContentView.swift
+++ NearbyRestaurantBrochure/NearbyRestaurantBrochure/ContentView.swift
@@ -2,17 +2,35 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let locationManager = LocationManager()
+    @StateObject private var locationManager = LocationManager()
+    @State private var shops: [Shop] = []
     
     var body: some View {
-        Text("")
-            .onAppear {
-                locationManager.startUpdating()
+        VStack {
+            
+            if let firstShop = shops.last {
+                Text(firstShop.name ?? "名前なし")
+                    .font(.title)
+            } else {
+                Text("お店を取得中...")
             }
+        }
+        .onAppear {
+            locationManager.startUpdating()
+        }
+        .onChange(of: locationManager.coordinate) { coordinate in
+            guard let coordinate else { return }
+            
+            Task {
+                do {
+                    shops = try await APIClient().fetchShops(coordinate: coordinate)
+                } catch {
+                    print("APIエラー:", error)
+                }
+            }
+        }
     }
 }
-
-
 
 #Preview {
     ContentView()
