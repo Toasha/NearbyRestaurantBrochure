419967f7f5ff725d66eedfb5207ad3fc752bb84a
Githubのユーザー名 <syoudai06180502@gmail.com>
2026-02-22 19:55:19 +0900

    hoge commit

--- NearbyRestaurantBrochure/NearbyRestaurantBrochure/Views/FilterSheetView.swift
+++ NearbyRestaurantBrochure/NearbyRestaurantBrochure/Views/FilterSheetView.swift
@@ -16,92 +16,91 @@ struct FilterSheetView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            VStack(spacing: 32) {
-                
-                //MARK: -Slider
-                VStack(alignment: .leading, spacing: 0) {
-                    HStack {
-                        Text("検索半径")
-                            .font(.headline)
-                        Spacer()
-                        Text("\(viewModel.selectedRange.displayText)")
-                            .padding()
-                            .foregroundStyle(.orange)
-                    }
+            VStack{
+                VStack(spacing: 32) {
                     
-                    Slider(
-                        value: Binding(
-                            get: { Double(viewModel.selectedRange.rawValue) },
-                            set: { viewModel.selectedRange = SearchRange(rawValue: Int($0)) ?? .km1 }
-                        ),
-                        in: 1...5,
-                        step: 1
-                    )
-                    .tint(.orange)
-                    
-                    HStack {
-                        ForEach(SearchRange.allCases) { range in
-                            Text(range.displayText)
-                                .font(.caption2)
-                                .foregroundColor(.secondary)
-                                .frame(maxWidth: .infinity)
+                    //MARK: -Slider
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("検索半径")
+                                .font(.headline)
+                            Spacer()
+                            Text("\(viewModel.selectedRange.displayText)")
+                                .foregroundStyle(.orange)
+                        }
+                        
+                        Slider(
+                            value: Binding(
+                                get: { Double(viewModel.selectedRange.rawValue) },
+                                set: { viewModel.selectedRange = SearchRange(rawValue: Int($0)) ?? .km1 }
+                            ),
+                            in: 1...5,
+                            step: 1
+                        )
+                        .tint(.orange)
+                        
+                        HStack {
+                            ForEach(SearchRange.allCases) { range in
+                                Text(range.displayText)
+                                    .font(.caption2)
+                                    .foregroundColor(.secondary)
+                                    .frame(maxWidth: .infinity)
+                            }
                         }
                     }
+                    
+                    //MARK: - acquisitions number
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("取得件数")
+                                .font(.headline)
+                            Spacer()
+                            Text("\(selection)件")
+                                .foregroundStyle(.orange)
+                        }
+                        Picker("取得件数", selection: $selection) {
+                            Text("10").tag(10)
+                            Text("20").tag(20)
+                            Text("30").tag(30)
+                            Text("40").tag(40)
+                            Text("50").tag(50)
+                            Text("100").tag(100)
+                        }
+                        .pickerStyle(.segmented)
+                        .tint(.orange)
+                    }
                 }
+                .padding(.horizontal, 24)
+                .padding(.top, 24)
+            Spacer()
+            //MARK: -　actionButton
+            HStack(spacing: 16) {
+                Button("リセット") {
+                    viewModel.selectedRange = .km1
+                    selection = 10
+                }
+                .font(.body.bold())
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .foregroundStyle(.orange)
+                .background(Color(.systemGray6))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
                 
-                //MARK: - acquisitions number
-                VStack(alignment: .leading, spacing: 0) {
-                    HStack {
-                        Text("取得件数")
-                            .font(.headline)
-                        Spacer()
-                        Text("\(selection)件")
-                            .foregroundStyle(.orange)
-                            .padding()
-                    }
-                    Picker("取得件数", selection: $selection) {
-                        Text("10件").tag(10)
-                        Text("20件").tag(20)
-                        Text("30件").tag(30)
-                        Text("40件").tag(40)
-                        Text("50件").tag(50)
-                    }
-                    .pickerStyle(.segmented)
-                    .tint(.orange)
+                Button("絞り込む") {
+                    // API再実行はここで
                 }
+                .font(.body.bold())
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .foregroundStyle(.white)
+                .background(Color.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .shadow(color: .orange.opacity(0.3), radius: 5, x: 0, y: 3)
             }
             .padding(.horizontal, 24)
-            .padding(.top, 24)
-        }
-        Spacer()
-        //MARK: -　actionButton
-        HStack(spacing: 16) {
-            Button("リセット") {
-                viewModel.selectedRange = .km1
-                selection = 10
+            .padding(.top, 16)
+            .padding(.bottom, 24)
             }
-            .font(.body.bold())
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 16)
-            .foregroundStyle(.orange)
-            .background(Color(.systemGray6))
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-            
-            Button("絞り込む") {
-                // API再実行はここで
-            }
-            .font(.body.bold())
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 16)
-            .foregroundStyle(.white)
-            .background(Color.orange)
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-            .shadow(color: .orange.opacity(0.3), radius: 5, x: 0, y: 3)
-        }
-        .padding(.horizontal, 24)
-        .padding(.top, 16)
-        .padding(.bottom, 24)
     }
 }
 
